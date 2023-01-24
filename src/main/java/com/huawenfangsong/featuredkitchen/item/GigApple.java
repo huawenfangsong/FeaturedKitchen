2f546481be0a5df8932cbabf17f18371aac96ccd
yuan_se <2106781810@qq.com>
2023-01-24 15:54:33 +0800

    Update GigApple.java

--- src/main/java/com/huawenfangsong/featuredkitchen/item/GigApple.java
+++ src/main/java/com/huawenfangsong/featuredkitchen/item/GigApple.java
@@ -16,7 +16,7 @@ public class GigApple extends Item {
 
     @Override
     public ItemStack finishUsingItem(ItemStack p_41409_, Level p_41410_, LivingEntity p_41411_){
-        p_41410_.playSound(p_41411_,p_41411_.getOnPos(), ModSoundEvents.PLAYER_EAT_GIGAPPLE.get(), SoundSource.PLAYERS,1f,1f);
+        p_41410_.playSound(p_41411_,p_41411_.getOnPos(), ModSoundEvents.PLAYER_EAT_GIGAPPLE.get(), SoundSource.MUSIC,1f,1f);
         if(p_41411_ instanceof Player){
             ((Player) p_41411_).getCooldowns().addCooldown(p_41409_.getItem(),140);
         }
